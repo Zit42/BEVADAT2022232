118588c03227266616c2ec0fa449562fff04c4e4
Zit42 <zita@stud.uni-obuda.hu>
2023-03-04 11:38:52 +0100

    Is there a dumb sum too?

--- GYAK/GYAK01/GYAK01.py
+++ GYAK/GYAK01/GYAK01.py
@@ -3,6 +3,7 @@
 #function name must be: contains_odd
 #input parameters: input_list
 
+#1
 def contains_odd(input_list):
     for xc in input_list:
         if xc%2 != 0:
@@ -16,6 +17,7 @@ def contains_odd(input_list):
 #function name must be: is_odd
 #input parameters: input_list
 
+#2
 def is_odd(input_list):
     res = list(map(lambda x: x%2!=0, input_list))
 #    for xc in input_list:
@@ -31,8 +33,19 @@ def is_odd(input_list):
 #function name must be: element_wise_sum
 #input parameters: input_list_1, input_list_2
 
+#3
 def element_wise_sum(input_list_1, input_list_2):
     res = list(map(lambda x,y: x+y, input_list_1, input_list_2))
+    if len(input_list_1)>len(input_list_2):
+        cnt=len(input_list_2)
+        while cnt<len(input_list_1):
+            res.append(input_list_1[cnt])
+            cnt+=1
+    elif len(input_list_2)>len(input_list_1):
+        cnt=len(input_list_1)
+        while cnt<len(input_list_2):
+            res.append(input_list_2[cnt])
+            cnt+=1
     return res
 
 #Create a function that accepts a dictionary and returns its items as a list of tuples
@@ -41,6 +54,7 @@ def element_wise_sum(input_list_1, input_list_2):
 #function name must be: dict_to_list
 #input parameters: input_dict
 
+#4
 def dict_to_list(input_dict):
     res = [(k,v) for k,v in input_dict.items()]
     #for xc in input_dict:
