d4f916491a2ff99018443e39c2c9e83551d7ba04
Zit42 <zita@stud.uni-obuda.hu>
2023-03-28 22:13:46 +0200

    updated leiras

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -48,7 +48,9 @@ class KNNClassifier:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
     
-    def plot_confusion_matrix(self) -> None:
+    def confusion_matrix(self) -> np.ndarray:
         conf_matrix = confusion_matrix(self.y_test, self.y_preds)
-        sns.heatmap(conf_matrix, annot=True)
+        #sns.heatmap(conf_matrix, annot=True)
+        return conf_matrix
+
 
